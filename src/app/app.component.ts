6de66e1e8d3760fbfa95370106853c14ac922987
ZCuak <gejomial@gmail.com>
2025-04-13 23:10:36 -0500

    Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TestService } from './services/test.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'itinerario-frontend';
+  backendResponse: any;
+  error: string = '';
+
+  constructor(private testService: TestService) {}
+
+  ngOnInit() {
+    this.testBackend();
+  }
+
+  testBackend() {
+    this.testService.testBackend().subscribe({
+      next: (response) => {
+        this.backendResponse = response;
+        this.error = '';
+      },
+      error: (err) => {
+        this.error = 'Error al conectar con el backend: ' + err.message;
+        this.backendResponse = null;
+      }
+    });
+  }
 }
